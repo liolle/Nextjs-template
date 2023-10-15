74d60719ef814074002cc3bbabc9b4f940a4e050
liolle <laurentlionel20@hotmail.com>
2023-10-15 19:56:35 +0200

    [Update] base db setup: - base sign up setting using next auth

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,7 +3,7 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
 // const connectionString = process.env.DATABASE_URL as string;
-const connectionString = "postgres://postgres:postgres@localhost:5432/atlas";
+const connectionString = "postgres://postgres:postgres@localhost:5432/postgres";
 const sql = postgres(connectionString, { max: 1 });
 const db = drizzle(sql);
 
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,74 +1,62 @@
-import { relations, sql } from "drizzle-orm";
 import {
-    pgTable,
-    smallint,
-    text,
     timestamp,
-    unique,
-    varchar
+    pgTable,
+    text,
+    primaryKey,
+    integer
 } from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "@auth/core/adapters";
+import { sql } from "drizzle-orm";
 
-// Schemas
-
-export const Users = pgTable("Users", {
-    id: varchar("id", { length: 8 }).primaryKey(),
-    email: varchar("email", { length: 100 }).unique().notNull(),
-    username: varchar("username", { length: 100 }).unique().notNull(),
-    role: smallint("role")
-        .notNull()
-        .references(() => User_role.id, { onDelete: "set null" }),
+export const users = pgTable("user", {
+    id: text("id").notNull().primaryKey(),
+    name: text("name"),
+    email: text("email").notNull(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    image: text("image").default(
+        "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png"
+    ),
     created_at: timestamp("created_at").default(sql`now()`)
 });
 
-export const User_role = pgTable("User_role", {
-    id: smallint("id").primaryKey(),
-    name: varchar("name", { length: 50 }).notNull()
-});
-
-export const Accounts = pgTable(
-    "Accounts",
+export const accounts = pgTable(
+    "account",
     {
-        id: varchar("id", { length: 12 }).primaryKey(),
-        password: varchar("password", { length: 128 }),
-        user_id: varchar("user_id", { length: 8 })
+        userId: text("userId")
             .notNull()
-            .references(() => Users.id, { onDelete: "cascade" }),
-        type: smallint("type")
-            .notNull()
-            .references(() => Account_type.id, { onDelete: "set null" }),
-        picture: text("picture")
-            .notNull()
-            .default(
-                "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png"
-            )
+            .references(() => users.id, { onDelete: "cascade" }),
+        type: text("type").$type<AdapterAccount["type"]>().notNull(),
+        provider: text("provider").notNull(),
+        providerAccountId: text("providerAccountId").notNull(),
+        refresh_token: text("refresh_token"),
+        access_token: text("access_token"),
+        expires_at: integer("expires_at"),
+        token_type: text("token_type"),
+        scope: text("scope"),
+        id_token: text("id_token"),
+        session_state: text("session_state")
     },
-    (t) => ({
-        id_type: unique().on(t.user_id, t.type)
+    (account) => ({
+        compoundKey: primaryKey(account.provider, account.providerAccountId)
     })
 );
 
-export const Account_type = pgTable("Account_type", {
-    id: smallint("id").primaryKey(),
-    name: varchar("name", { length: 50 }).notNull()
+export const sessions = pgTable("session", {
+    sessionToken: text("sessionToken").notNull().primaryKey(),
+    userId: text("userId")
+        .notNull()
+        .references(() => users.id, { onDelete: "cascade" }),
+    expires: timestamp("expires", { mode: "date" }).notNull()
 });
 
-// Relations
-
-export const UserRelations = relations(Users, ({ one, many }) => ({
-    role: one(User_role, {
-        fields: [Users.role],
-        references: [User_role.id]
-    }),
-    accounts: many(Accounts)
-}));
-
-export const AccountRelation = relations(Accounts, ({ one }) => ({
-    user: one(Users, {
-        fields: [Accounts.user_id],
-        references: [Users.id]
-    }),
-    type: one(Account_type, {
-        fields: [Accounts.type],
-        references: [Account_type.id]
+export const verificationTokens = pgTable(
+    "verificationToken",
+    {
+        identifier: text("identifier").notNull(),
+        token: text("token").notNull(),
+        expires: timestamp("expires", { mode: "date" }).notNull()
+    },
+    (vt) => ({
+        compoundKey: primaryKey(vt.identifier, vt.token)
     })
-}));
+);
