225e4293631334e2058e5bc59e398c81f45cdbd1
liolle <laurentlionel20@hotmail.com>
2023-10-15 19:57:51 +0200

    [Update] zod type: - adding zod form verification for EmailRegistration

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
 /**
  * TYPES
  */
+import * as z from "zod";
 
 export type ExampleType = "Next";
 export const TEMPLATE_VERSION = "v.0.0.5";
+
+const EmailRegistration = z.object({
+    email: z.string().email()
+});
+
+export { EmailRegistration };
